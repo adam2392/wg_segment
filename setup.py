4e3d7ddd8c2cf0eeed4def955d609051f1d02524
Adam Li <adam2392@gmail.com>
2020-02-11 10:35:23 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A set of research and models for white/gray matter segmentation using only LFP signals.',
+    author='Adam, Chester, Patrick',
+    license='MIT',
+)
