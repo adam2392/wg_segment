5ac951860b3a33dc66cafc04841b2b90af6471d5
ChesterHuynh <chester.huynh924@gmail.com>
2020-02-24 16:17:19 -0500

    Separated visualization functions

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,12 +1,12 @@
 import collections
 import os
 import re
+import sys
+
+sys.path.append("../..")
 
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import mne
-import seaborn as sns
 
 from mne import io
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,15 +15,19 @@ from pathlib import Path
 from random import randrange
 from rerf.rerfClassifier import rerfClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 from typing import Dict, List, Union
 
+from random_forest_visualization import generate_roc_curve, generate_precision_recall_curve
 from mne_bids.utils import print_dir_tree, _parse_bids_filename
 from mne_bids import write_raw_bids, read_raw_bids, make_bids_basename, make_bids_folders
 from mne_bids.tsv_handler import _from_tsv, _to_tsv
 
 from sample_scripts import file_utils
+
+
+# sns.set_context("paper", font_scale=1.5)
 
 
 def load_elecs_data(elecfile: Union[str, Path]):
@@ -77,64 +81,44 @@ def load_elecs_data(elecfile: Union[str, Path]):
 
 
 def make_seeg_montage_edf(subject: str, edf_fpath: str, elecs_fpath: str):
+    """
+    Construct a SEEG montage from .edf data.
+    """
     missing_chs = []
+
     if subject == "la03":
         missing_chs = ["Y16", "R'1", "R'2", "R'3", "R'4",
                        "R'5", "R'6", "R'7", "R'8", "R'9",
                        "X'1", "X'2", "X'9", "X'10"]  # for la03
+
+    # Read .edf file into mne Raw object
     raw = io.read_raw_edf(edf_fpath, preload=True, verbose=False)
 
+    # Setting channel types
     raw = file_utils._set_channel_types(raw, verbose=False)
+
+    # Standardizing channel labels
     raw = file_utils.channel_text_scrub(raw)
 
+    # Scrub out bad channels
     chan_scrub = file_utils.ChannelScrub()
     bad_chs = chan_scrub.look_for_bad_channels(raw.ch_names)
     raw.info["bads"] = bad_chs + missing_chs
+
+    # Exclude bad channels from raw object
     raw.pick_types(eeg=True, exclude="bads")
+
+    # Construct montage
     montage = mne.channels.make_dig_montage(ch_pos=ch_pos, coord_frame="mri")
+
     return raw, montage
 
 
-def run_rerf(train: np.ndarray, target: np.ndarray, fig_dir: str):
-    mtry = [4, 3, 2, 1]
-    mtryMult = [round(i/100 + 0.4, 1) for i in range(0, 400, 40)]
-    parameters = {'max_features': mtry, "feature_combinations": mtryMult}
-    rf = rerfClassifier(projection_matrix="RerF")
-    clf = GridSearchCV(rf, parameters, cv=5)
-    clf.fit(train, target)
-
-    mtry = np.asarray(mtry)
-    Lambda = np.asarray(mtryMult)
-    r = np.asarray(clf.cv_results_['rank_test_score']).reshape(len(mtry),len(mtryMult))
-
-    mpl.rc('figure', figsize=[12,8], dpi=300)
-    fig, ax = plt.subplots(figsize=(8,6))
-    im = ax.imshow(r, cmap='Greens')
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(Lambda)))
-    ax.set_yticks(np.arange(len(mtry)))
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(Lambda)
-    ax.set_yticklabels(mtry)
-    plt.xlabel('mtryMult')
-    plt.ylabel('mtry')
-
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(mtry)):
-        for j in range(len(Lambda)):
-            text = ax.text(j, i, r[i, j],
-                           ha="center", va="center", color="black")
-
-    ax.set_title("Parameter sweep ranks (lower is better)")
-    fig.tight_layout()
-
-    fpath = os.path.join(fig_dir, "param_sweeps_rerf.png")
-
-    plt.savefig(fpath)
-
-
 def find_bipolar_reference(ch_names):
+    """
+    Get corresponding lists of paired anode and cathode channels as well as
+    any channels that are missing their neighboring channel.
+    """
     ch_names = natsorted(ch_names)
 
     # get all unique electrodes
@@ -162,13 +146,13 @@ def find_bipolar_reference(ch_names):
         n = len(ch_list)
         ch_list = np.array(ch_list)
 
-        for (ch_num0, ch_num1) in zip(ch_list[:n-2], ch_list[1:n-1]):
-            
+        for (ch_num0, ch_num1) in zip(ch_list[0:n-1], ch_list[1:n]):
+
             if int(ch_num0) == int(ch_num1) - 1:
-            
+
                 anode_chs.append(f"{_elec_name}{ch_num0}")
                 cathode_chs.append(f"{_elec_name}{ch_num1}")
-            
+
             else:
                 # Adjacent channel does not exist
                 monopolar_chs.append(f"{_elec_name}{ch_num1}")
@@ -177,6 +161,10 @@ def find_bipolar_reference(ch_names):
 
 
 def get_monopolar_data(raw, ch_labels):
+    """
+    Construct signal time series with original monopolar channel signals.
+    Corresponding white matter/grey matter labels are also returned.
+    """
     ch_names = raw.ch_names
 
     X = raw.get_data()
@@ -185,108 +173,145 @@ def get_monopolar_data(raw, ch_labels):
     return X, y
 
 
-def get_bipolar_data(raw, ch_labels, anode, cathode, monopolar=[]):
+def get_bipolar_data(
+    raw,
+    ch_labels,
+    anode,
+    cathode,
+    monopolar=[],
+    include_monopolar=True
+):
+    """
+    Construct signal time series by subtracting adjacent channel signals.
+    Corresponding white matter/grey matter labels for the anode channels are
+    also returned.
+    """
     anode_data = raw.get_data(anode)
     cathode_data = raw.get_data(cathode)
-    mono_data = raw.get_data(monopolar) if monopolar is not None else None
+    mono_data = raw.get_data(monopolar) if len(monopolar) else None
 
     X = anode_data - cathode_data
     y = np.array([ch_labels[ch] for ch in anode])
-    
-    if mono_data is not None:
+
+    if mono_data is not None and include_monopolar:
         X = np.append(X, mono_data, axis=0)
-        
+
         mono_labels = np.array([ch_labels[ch] for ch in monopolar])
         y = np.append(y, mono_labels)
-    
+
     return X, y
 
 
-def get_averaged_data(raw: mne.io.Raw, ch_labels: Dict):
-    ch_names = natsorted(ch_labels.keys())
-
-    # get all unique electrodes
-    elec_names = []
-    for x in ch_names:
-
-        elec_name = re.sub("[0-9]", "", x)
-
-        if elec_name not in elec_names:
-            elec_names.append(elec_name)
-
-    # get the channel numbers for each electrode
-    elec_to_channels = collections.defaultdict(list)
-    for x in ch_names:
-        elec, num = re.match("^([A-Za-z]+[']?)([0-9]+)$", x).groups()
-        elec_to_channels[elec].append(elec + str(num))
+def get_averaged_data(raw: mne.io.Raw, ch_labels: Dict, by_electrode=False):
+    """
+    Construct signal time series by subtracting mean signal for each electrode.
+    Corresponding white matter/grey matter labels are also returned.
+    """
 
     X = None
     y = None
 
-    for elec in elec_to_channels:
-        chans_list = elec_to_channels[elec]
-        
-        labels = [ch_labels[ch] for ch in chans_list]
-        labels = np.array(labels)
-        
-        # Subtract out average signal
-        elec_data = raw.get_data(chans_list)
-        avg_signal = np.mean(elec_data, axis=0)
-        centered_data = elec_data - avg_signal
-        
-        if (X is None) or (y is None):
-            X = centered_data
-            y = labels
-        else:
-            X = np.append(X, centered_data, axis=0)
-            y = np.append(y, labels)
-    
+    if not by_electrode:
+        ch_names = raw.ch_names
+        raw = raw.set_eeg_reference(ref_channels="average")
+
+        X = raw.get_data()
+        y = np.array([ch_labels[ch] for ch in ch_names])
+    else:
+        ch_names = natsorted(ch_labels.keys())
+
+        # get all unique electrodes
+        elec_names = []
+        for x in ch_names:
+
+            elec_name = re.sub("[0-9]", "", x)
+
+            if elec_name not in elec_names:
+                elec_names.append(elec_name)
+
+        # get the channel numbers for each electrode
+        elec_to_channels = collections.defaultdict(list)
+        for x in ch_names:
+            elec, num = re.match("^([A-Za-z]+[']?)([0-9]+)$", x).groups()
+            elec_to_channels[elec].append(elec + str(num))
+
+        for elec in elec_to_channels:
+            chans_list = elec_to_channels[elec]
+
+            labels = [ch_labels[ch] for ch in chans_list]
+            labels = np.array(labels)
+
+            # Subtract out average signal
+            elec_data = raw.get_data(chans_list)
+            avg_signal = np.mean(elec_data, axis=0)
+            centered_data = elec_data - avg_signal
+
+            if (X is None) or (y is None):
+                X = centered_data
+                y = labels
+            else:
+                X = np.append(X, centered_data, axis=0)
+                y = np.append(y, labels)
+
     return X, y
 
 
 def get_data_from_raw(
     raw: mne.io.Raw,
     elec_descrip: Dict,
-    window_size_seconds: int=10,
-    strided: bool=True,
-    reference: str='monopolar'
+    window_size_seconds: int = 10,
+    strided: bool = True,
+    reference: str = "monopolar",
+    *args,
+    **kwargs
 ):
+    """
+    Get time series data and corresponding white matter/grey matter labels
+    from Raw object.
+    """
     ch_names = raw.ch_names
-
-    n_chs = len(raw.ch_names)
+    reference = reference.lower()
     n_samples = int(window_size * raw.info["sfreq"])
 
+    # Get binary class labels for each channel
     ch_labels = {}
     for i in range(len(elec_descrip["status_description"])):
         name = elec_descrip["name"][i]
         status = elec_descrip["status"][i]
         descrip = elec_descrip["status_description"][i]
 
-        if status == "bad" and descrip == "white matter":    
+        if status == "bad" and descrip == "white matter":
             ch_labels[name] = 1
         elif name in ch_names:
             ch_labels[name] = 0
-
-    # TODO: test bipolar and averaged are correct
 
     if reference == "monopolar":
         X, y = get_monopolar_data(raw, ch_labels)
 
     elif reference == "bipolar":
+        include_monopolar = True
+        if "include_monopolar" in kwargs:
+            include_monopolar = kwargs["include_monopolar"]
+
         anode, cathode, mono = find_bipolar_reference(ch_names)
-        X, y = get_bipolar_data(raw, ch_labels, anode, cathode, mono)
+        X, y = get_bipolar_data(raw, ch_labels, anode, cathode, mono, include_monopolar)
 
     elif reference == "averaged":
-        X, y = get_averaged_data(raw, ch_labels)
-    
+        by_electrode = False
+        if "by_electrode" in kwargs:
+            by_electrode = kwargs["by_electrode"]
+
+        X, y = get_averaged_data(raw, ch_labels, by_electrode)
+
     else:
         raise ValueError(f"Reference type {reference} not recognized")
 
+    # If strided, construct each sample with random windows of each channel,
+    # otherwise just use the same window for all channels
     if not strided:
         start = randrange(raw.n_times - n_samples + 1)
         stop = start + n_samples
         X = X[:, start:stop]
-
     else:
         m, n = X.shape
         start = np.random.randint(
@@ -305,53 +330,9 @@ def get_data_from_raw(
 
         X = windows[np.arange(len(start)), start]
 
+    print(f"Data dimensions - X.shape {X.shape}, y.shape {y.shape}")
+
     return X, y
-
-
-def generate_roc_curve(
-    y_test,
-    classifier_names,
-    prediction_probs,
-    fig_dir,
-    reference,
-    verbose=True
-):
-    n_clfs = len(classifier_names)
-    
-    fprs = []
-    tprs = []
-
-    fig, ax = plt.subplots(1, 1, dpi=300)
-
-    for i, (name, proba) in enumerate(zip(classifier_names, prediction_probs)):
-        if verbose:
-            auc = roc_auc_score(y_test, proba[:, 1])
-            print(f"{name} auc score: {auc}\n")
-        
-        fpr, tpr, _ = roc_curve(y_test, proba[:, 1])
-        
-        fprs.append(fpr)
-        tprs.append(tpr)
-
-        sns.lineplot(
-            x=fpr,
-            y=tpr,
-            label=f"{name} ROC curve (area = {auc:.3f})",
-            ci=None,
-            ax=ax
-        )
-
-    ax.set(
-        title=f"ROC Curves for Random Forest Variants with {reference} Data",
-        xlabel="False Positive Rate",
-        ylabel="True Positive Rate")
-
-    fig.tight_layout()
-    
-    fpath = os.path.join(fig_dir, f"roc_curves-{reference}.png")
-    plt.savefig(fpath)
-
-    return fprs, tprs
 
 
 def train_classifiers(
@@ -366,23 +347,24 @@ def train_classifiers(
     """
 
     clfs = []
+    n_samples = int(kwargs["window_size"] * kwargs["sfreq"])
 
     for clfname in classifier_names:
         name = clfname.lower()
         clf = None
 
         if name in ["rf", "random forest", "standard random forest"]:
-            clf = RandomForestClassifier(n_estimators=100)
+            clf = RandomForestClassifier(n_estimators=500)
 
         elif name in ["rerf", "random project random forest"]:
-            clf = rerfClassifier(n_estimators=100)
+            clf = rerfClassifier(n_estimators=500)
 
         elif name in ["srerf", "sporf", "structured rerf"]:
             clf = rerfClassifier(
                 projection_matrix="S-RerF",
-                n_estimators=100,
+                n_estimators=500,
                 image_height=1,
-                image_width=int(kwargs["window_size"] * kwargs["sfreq"]),
+                image_width=n_samples,
                 patch_height_max=1,
                 patch_height_min=1
             )
@@ -417,18 +399,19 @@ def predict_classifiers(
     prediction_probs.append(zero_pred_proba)
 
     if verbose:
-        print(f"Zero classifier accuracy: {accuracy_score(y_test, zero_pred)}\n")
-    
+        print(f"Zero classifier accuracy: {accuracy_score(y_test, zero_pred)}")
+
     for name, clf in classifiers.items():
 
         ypred = clf.predict(X_test)
         proba = clf.predict_proba(X_test)
-        
+
         predictions.append(ypred)
         prediction_probs.append(proba)
 
         if verbose:
-            print(f"{name} accuracy: {accuracy_score(y_test, ypred)}\n")
+            print(f"{name} accuracy: {accuracy_score(y_test, ypred)}")
+    print()
 
     return predictions, prediction_probs
 
@@ -488,8 +471,6 @@ if __name__ == "__main__":
     fif_dir = os.path.join(sourcedata, acq, "fif")
     elecs_dir = os.path.join(derivatives, "elecs")
 
-    fig_dir = "/workspaces/ChesterHuynh/research/seeg localization/figs/"
-
     # Get relevant file paths
     elec_layout_fpath = file_utils._get_pt_electrode_layout(sourcedata, patid)
 
@@ -514,34 +495,48 @@ if __name__ == "__main__":
     raw = raw.set_montage(montage)
 
     # Data processing
-    window_size = 10  # in seconds
-    reference = "averaged"
+    window_sizes = [5, 10, 15]  # in seconds
+    references = ["averaged", "bipolar", "monopolar"]
 
-    X, y = get_data_from_raw(
-        raw,
-        elec_descrip,
-        window_size_seconds=window_size,
-        strided=True,
-        reference=reference
-    )
+    for window_size in window_sizes:
+        for reference in references:
+            X, y = get_data_from_raw(
+                raw,
+                elec_descrip,
+                window_size_seconds=window_size,
+                strided=True,
+                reference=reference,
+                by_electrode=False,
+                include_monopolar=False
+            )
 
-    # Split dataset into training set and test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
-    classifier_names = ["Random Forest", "RerF", "SPORF"]
-    predictions, prediction_probs = evaluate_classifiers(
-        X_train, y_train,
-        X_test, y_test,
-        classifier_names,
-        window_size=window_size,
-        sfreq=raw.info["sfreq"]
-    )
+            # Split dataset into training set and test set
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+            classifier_names = ["Random Forest", "RerF", "SPORF"]
+            predictions, prediction_probs = evaluate_classifiers(
+                X_train, y_train,
+                X_test, y_test,
+                classifier_names,
+                window_size=window_size,
+                sfreq=raw.info["sfreq"]
+            )
 
-    classifier_names = ["Zero"] + classifier_names
+            fig_dir = f"/workspaces/ChesterHuynh/research/seeg localization/figs/{window_size}sec/"
 
-    fprs, tprs = generate_roc_curve(
-        y_test,
-        classifier_names,
-        prediction_probs,
-        fig_dir,
-        reference
-    )
+            classifier_names = ["Zero"] + classifier_names
+
+            fprs, tprs = generate_roc_curve(
+                y_test,
+                classifier_names,
+                prediction_probs,
+                fig_dir,
+                reference
+            )
+
+            precisions, recalls = generate_precision_recall_curve(
+                y_test,
+                classifier_names,
+                prediction_probs,
+                fig_dir,
+                reference
+            )
---
+++ models/random_forest_visualization.py
@@ -0,0 +1,108 @@
+import os
+
+import seaborn as sns
+
+from matplotlib import pyplot as plt
+from sklearn.metrics import precision_recall_curve, roc_auc_score, roc_curve
+
+
+def generate_roc_curve(
+    y_test,
+    classifier_names,
+    prediction_probs,
+    fig_dir,
+    reference,
+    verbose=True
+):
+    """
+    Plot ROC curves and report accompanying AUC.
+    """
+
+    fprs = []
+    tprs = []
+
+    fig, ax = plt.subplots(1, 1, dpi=300)
+
+    for i, (name, proba) in enumerate(zip(classifier_names, prediction_probs)):
+        if verbose:
+            auc = roc_auc_score(y_test, proba[:, 1])
+            print(f"{name} ROC auc score: {auc}")
+
+        fpr, tpr, _ = roc_curve(y_test, proba[:, 1])
+
+        fprs.append(fpr)
+        tprs.append(tpr)
+
+        sns.lineplot(
+            x=fpr,
+            y=tpr,
+            label=f"{name} ROC curve (area = {auc:.3f})",
+            ci=None,
+            ax=ax
+        )
+
+    ax.set(
+        title=f"ROC Curves for Random Forest Variants with {reference.capitalize()} Data",
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate")
+
+    fig.tight_layout()
+
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+
+    fpath = os.path.join(fig_dir, f"roc_curves-{reference.lower()}.png")
+    plt.savefig(fpath)
+
+    return fprs, tprs
+
+
+def generate_precision_recall_curve(
+    y_test,
+    classifier_names,
+    prediction_probs,
+    fig_dir,
+    reference,
+    verbose=True
+):
+    """
+    Plot precision recall curves and report accompanying AUC.
+    """
+
+    precisions = []
+    recalls = []
+
+    fig, ax = plt.subplots(1, 1, dpi=300)
+
+    for i, (name, proba) in enumerate(zip(classifier_names, prediction_probs)):
+        precision, recall, _ = precision_recall_curve(y_test, proba[:, 1])
+
+        precisions.append(precision)
+        recalls.append(recall)
+
+        if verbose:
+            pr_auc = auc(recall, precision)
+            print(f"{name} PR auc score: {pr_auc}")
+
+        sns.lineplot(
+            x=recall,
+            y=precision,
+            label=f"{name} precision-recall curve (area = {pr_auc:.3f})",
+            ci=None,
+            ax=ax
+        )
+
+    ax.set(
+        title=f"Precision Recall Curves for Random Forest Variants with {reference.capitalize()} Data",
+        xlabel="Recall",
+        ylabel="Precision")
+
+    fig.tight_layout()
+
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+
+    fpath = os.path.join(fig_dir, f"pr_curves-{reference.lower()}.png")
+    plt.savefig(fpath)
+
+    return precisions, recalls
